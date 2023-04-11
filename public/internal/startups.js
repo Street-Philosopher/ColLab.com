8efb9d376f4f32a765261e47d495229983d9ab40
leo-ag <leoniantonio0@gmail.com>
2023-04-11 08:11:35 +0200

    updated to firebase

---
+++ public/internal/startups.js
@@ -0,0 +1,11 @@
+
+function internal_get_startups() {
+	list = {
+		1: { name: "ColLab", link: "#", description: "Pubblicizza la tua startup, trova persone interessate e falla crescere!", image: "../image/logo.jpeg" },
+		2: { name: "Tavolità", link: "#", description: "Banchi scolastici sostenibili, nati da scarti plastici, in un'economia circolare.", image:null },
+		3: { name: "OWine", link: "#", description: "Un nuovo metodo di ossigenazione del vino, che fa risparmiare tempo e dà ottimi risultati.", image:null },
+		4: { name: "Startup Esempio", link: "#", description: "Questo è un esempio per mostrare il funzionamento del sito.", image:null },
+		5: { name: "Un altro esempio", link: "#", description: "Questa breve descrizione descrive la startup.", image:null },
+	}
+	return list;
+}
